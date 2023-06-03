79a2a5ded0c3c3eb50144b49030c1a1f0abb31d8
glebk <glebkot57@gmail.com>
2023-06-03 17:02:29 +0200

    1

--- src/channel.py
+++ src/channel.py
@@ -8,7 +8,7 @@ class Channel:
 
     api_key: str = "AIzaSyBIE1Zoz-q-0QXM1H8hp3e_N9xnuT_9xfI"
     youtube = build('youtube', 'v3', developerKey=api_key)
-    __channel_id: str
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
@@ -25,6 +25,7 @@ class Channel:
         except Exception as e:
             print("Error", str(e))
 
+
     @classmethod
     def get_service(cls):
         """Возвращает объект для работы с YouTube API"""
@@ -35,15 +36,15 @@ class Channel:
         """Выводит в консоль информацию о канале."""
 
 
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     def to_json(self, file_name):
         """Сохраняет в файл значения атрибутов экземпляра `Channel`"""
-        data = self.__dict__
+
 
         with open(file_name, 'w', encoding='utf-8') as file:
-            json.dump(data, file)
+            json.dump(self.__dict__, file, ensure_ascii=False)
 
 
