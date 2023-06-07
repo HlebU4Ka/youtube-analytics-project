d85465a79df2036e58f02568939063e12ae907a1
glebk <glebkot57@gmail.com>
2023-06-07 06:29:05 +0200

    create code

--- src/video.py
+++ src/video.py
@@ -1,16 +1,46 @@
+from googleapiclient.discovery import build
+
 
 class Video:
+    """Класс для ютуб-канала"""
 
-    def __init__(self, id_video, name_video, link_video, view_count, view_likes ):
+    api_key: str = "AIzaSyBIE1Zoz-q-0QXM1H8hp3e_N9xnuT_9xfI"
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, id_video, ):
+        """
+        Создайте файл src/video и в нем класс Video
+        Реализуйте инициализацию реальными данными следующих атрибутов экземпляра класса Video:
+        id видео
+        название видео
+        ссылка на видео
+        количество просмотров
+        количество лайков
+        """
         self.id_video = id_video
-        self.name_video = name_video
-        self.link_video = link_video
-        self.view_count = int(view_count)
-        self.view_likes = int(view_likes)
+        try:
+            response = self.youtube.videos().list(
+                part='snippet, statistics',
+                id=self.id_video
+            ).execute()
+            video = response['item'][0]
+
+            self.title = video['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.id_video
+            self.view_count = video['statistics']['viewCount']
+            self.view_likes = video['statistics']['likeCount']
+        except Exception as e:
+            print("Error", str(e))
+
+    def __str__(self):
+        return self.title
 
 
-
-class PLVideo:
-    def __init__(self, video_id, title, video_link, views, likes, playlist_id):
-        super().__init__(video_id, title, video_link, views, likes)
+class PLVideo(Video):
+    def __init__(self, playlist_id, id_video):
+        super().__init__(id_video)
         self.playlist_id = playlist_id
+
+    def __str__(self):
+        return self.title
+
